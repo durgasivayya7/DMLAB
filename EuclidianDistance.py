4c03ab34a40a8d99339a220750e0ffa4edf0f0e8
K .  Durga Siva <durgasivayyakarri@gmail.com>
2025-11-12 00:16:39 +0530

    Update EuclidianDistance.py

--- EuclidianDistance.py
+++ EuclidianDistance.py
@@ -1,9 +1,14 @@
 import math
-
-data = [[1,2],[2,3],[3,5],[6,8]]
-n = len(data)
-matrix = [[round(math.sqrt((data[i][0]-data[j][0])**2 + (data[i][1]-data[j][1])**2),2)
-           for j in range(n)] for i in range(n)]
-
-print("Dissimilarity Matrix:")
-for row in matrix: print(row)
+n = int(input("Enter number of data points: "))
+data = []
+for i in range(n):
+    x, y = map(float, input(f"Enter point {i+1} (x,y): ").split(','))
+    data.append([x, y])
+matrix = [
+    [round(math.sqrt((data[i][0] - data[j][0]) ** 2 + (data[i][1] - data[j][1]) ** 2), 2)
+     for j in range(n)]
+    for i in range(n)
+]
+print("\nDissimilarity Matrix:")
+for row in matrix:
+    print(row)
